f245ebd4cceb6c7726bd0b2867d41616bd16cd0c
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-30 22:54:50 -0300

    Feat: finishing Product Entity

--- src/main/java/com/jotace/inventory_project/model/database/Product.java
+++ src/main/java/com/jotace/inventory_project/model/database/Product.java
@@ -1,13 +1,18 @@
 package com.jotace.inventory_project.model.database;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.jotace.inventory_project.model.request.ProductRequest;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 
 @Data
+@NoArgsConstructor
+@DynamoDBTable(tableName = "Products")
 public class Product {
 
     @DynamoDBHashKey
@@ -23,6 +28,7 @@ public class Product {
     private String description;
 
     public Product(ProductRequest productRequest) {
+        this.id = UUID.randomUUID().toString();
         this.name = productRequest.name();
         this.price = productRequest.price();
         this.description = productRequest.description();
