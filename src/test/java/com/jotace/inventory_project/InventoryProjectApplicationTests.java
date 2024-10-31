f537ec00a5fac7e5fe5aa96935cdddd0a464bc4f
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-30 21:57:39 -0300

    Feat: create project

---
+++ src/test/java/com/jotace/inventory_project/InventoryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotace.inventory_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
