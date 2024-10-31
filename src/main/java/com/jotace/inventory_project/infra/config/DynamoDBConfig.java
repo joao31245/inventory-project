33b3b037cf307fa45c3e69ac05a25b7cb4fcea68
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-30 22:52:38 -0300

    Feat: using the region from the properties file

--- src/main/java/com/jotace/inventory_project/infra/config/DynamoDBConfig.java
+++ src/main/java/com/jotace/inventory_project/infra/config/DynamoDBConfig.java
@@ -2,16 +2,19 @@ package com.jotace.inventory_project.infra.config;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDBConfig {
+    @Value("${aws.region}")
+    private String region;
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
-                .withRegion("us-east-1") // Replace with your region
+                .withRegion(region) // Replace with your region
                 .build();
     }
 }
