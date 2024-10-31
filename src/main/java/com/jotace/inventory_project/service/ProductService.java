fc46e697d3b546c64e939bdb4adfc8a4b541ce84
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-30 22:10:26 -0300

    Feat: Create Product Service

---
+++ src/main/java/com/jotace/inventory_project/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.jotace.inventory_project.service;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.jotace.inventory_project.model.database.Product;
+import com.jotace.inventory_project.model.request.ProductRequest;
+import com.jotace.inventory_project.model.request.ProductResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    public ProductService(DynamoDBMapper dynamoDBMapper) {
+        this.dynamoDBMapper = dynamoDBMapper;
+    }
+
+    public void save(ProductRequest productRequest) {
+        var product = new Product(productRequest);
+        dynamoDBMapper.save(product);
+    }
+
+    public ProductResponse findById(String id) {
+        var product = dynamoDBMapper.load(Product.class, id);
+
+        return new ProductResponse(product);
+    }
+
+    public void delete(String id) {
+        var product = dynamoDBMapper.load(Product.class, id);
+        dynamoDBMapper.delete(product);
+    }
+}
