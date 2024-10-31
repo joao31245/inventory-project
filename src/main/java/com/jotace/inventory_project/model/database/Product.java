07efbd02b28a4419bdb399617ff1b595833aeb9c
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-31 19:47:28 -0300

    Feat: create new Product entity

--- src/main/java/com/jotace/inventory_project/model/database/Product.java
+++ src/main/java/com/jotace/inventory_project/model/database/Product.java
@@ -1,30 +1,24 @@
 package com.jotace.inventory_project.model.database;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
 import com.jotace.inventory_project.model.request.ProductRequest;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-
 @Data
 @NoArgsConstructor
-@DynamoDBTable(tableName = "Products")
+@DynamoDbBean
 public class Product {
 
-    @DynamoDBHashKey
-    private String id; // Primary key
+    private String id;
 
-    @DynamoDBAttribute
-    private String name; // Other attributes
-
-    @DynamoDBAttribute
+    private String name;
     private BigDecimal price;
-
-    @DynamoDBAttribute
     private String description;
 
     public Product(ProductRequest productRequest) {
@@ -32,5 +26,25 @@ public class Product {
         this.name = productRequest.name();
         this.price = productRequest.price();
         this.description = productRequest.description();
+    }
+
+    @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
+
+    @DynamoDbAttribute("name")
+    public String getName() {
+        return name;
+    }
+
+    @DynamoDbAttribute("price")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @DynamoDbAttribute("description")
+    public String getDescription() {
+        return description;
     }
 }
