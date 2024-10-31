497d3f7042063acfca7de2241f29e63ec1b098e2
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-31 19:32:11 -0300

    Feat: add DynamoDbMapper beans

--- src/main/java/com/jotace/inventory_project/infra/config/DynamoDBConfig.java
+++ src/main/java/com/jotace/inventory_project/infra/config/DynamoDBConfig.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 @Configuration
 public class DynamoDBConfig {
@@ -17,4 +18,10 @@ public class DynamoDBConfig {
                 .withRegion(region) // Replace with your region
                 .build();
     }
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+        return new DynamoDBMapper(amazonDynamoDB);
+    }
+
 }
