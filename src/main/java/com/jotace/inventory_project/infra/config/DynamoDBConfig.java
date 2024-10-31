83cb5976487d6b37aa34ea945c8da6e7756dbba1
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-31 19:47:12 -0300

    Feat: updating DynamoDb beans

--- src/main/java/com/jotace/inventory_project/infra/config/DynamoDBConfig.java
+++ src/main/java/com/jotace/inventory_project/infra/config/DynamoDBConfig.java
@@ -1,27 +1,27 @@
 package com.jotace.inventory_project.infra.config;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 @Configuration
 public class DynamoDBConfig {
-    @Value("${aws.region}")
-    private String region;
 
     @Bean
-    public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.standard()
-                .withRegion(region) // Replace with your region
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
     @Bean
-    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
-        return new DynamoDBMapper(amazonDynamoDB);
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient())
+                .build();
     }
-
 }
