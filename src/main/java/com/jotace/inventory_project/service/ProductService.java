1f6a5d7bb24fe775e6a408a71405687090dc941a
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-10-31 21:32:39 -0300

    Feat: add findAll method

--- src/main/java/com/jotace/inventory_project/service/ProductService.java
+++ src/main/java/com/jotace/inventory_project/service/ProductService.java
@@ -3,16 +3,19 @@ package com.jotace.inventory_project.service;
 import com.jotace.inventory_project.model.database.Product;
 import com.jotace.inventory_project.model.request.ProductRequest;
 import com.jotace.inventory_project.model.request.ProductResponse;
+import org.springframework.beans.factory.annotation.Value;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ProductService {
-
     private final DynamoDbEnhancedClient dynamoDbEnhancedClient;
 
     @Autowired
@@ -22,10 +25,11 @@ public class ProductService {
 
     private static final String TABLE_NAME = "Products";
 
-    public void save(ProductRequest productRequest) {
+    public ProductResponse save(ProductRequest productRequest) {
         var product = new Product(productRequest);
         var productTable = dynamoDbEnhancedClient.table(TABLE_NAME, TableSchema.fromBean(Product.class));
         productTable.putItem(product);
+        return new ProductResponse(product);
     }
 
     public ProductResponse findById(String id) {
@@ -38,5 +42,15 @@ public class ProductService {
     public void delete(String id) {
         var productTable = dynamoDbEnhancedClient.table(TABLE_NAME, TableSchema.fromBean(Product.class));
         productTable.deleteItem(Key.builder().partitionValue(id).build());
+    }
+
+    public List<ProductResponse> findAll() {
+        var productTable = dynamoDbEnhancedClient.table(TABLE_NAME, TableSchema.fromBean(Product.class));
+
+        var listProduct = productTable.scan();
+
+        return StreamSupport.stream(listProduct.items().spliterator(), false)
+                .map(ProductResponse::new)
+                .collect(Collectors.toList());
     }
 }
